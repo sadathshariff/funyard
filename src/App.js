1f5bf1fc1fe1e6d9b1d97be56057c30ac6b6c421
sadath <sadathshariff65@gmail.com>
2022-04-18 09:01:41 +0530

    exporting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 import { RouterPath } from "./routes/RouterPath";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Header, Sidenav } from "./components";
+
 function App() {
+  const [showSidenav, setShowSidenav] = useState(false);
+  const toggleSidenav = () => setShowSidenav((prev) => !prev);
   return (
-    <div className="App">
-      <RouterPath />
+    <div className="App ">
+      <Header toggleSidenav={toggleSidenav} />
+      <div className="main-container">
+        <Sidenav showSidenav={showSidenav} toggleSidenav={toggleSidenav} />
+        <div className="components-div">
+          <RouterPath />
+        </div>
+      </div>
+      <ToastContainer theme="dark" />
     </div>
   );
 }
