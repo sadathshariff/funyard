e0e89d22fc0d3812d5fdb84e1b2f9c4d42e10707
sadath <sadathshariff65@gmail.com>
2022-04-20 14:57:17 +0530

    Fix:"added alt  attribute to banner img"

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,18 +12,14 @@ export const Banner = () => {
     transitionDuration: 1000,
     easing: "ease",
   };
-  const fadeImages = [
-    Banner1,
-    Banner2,
-    Banner4,
-    // "https://images.unsplash.com/photo-1543584756-8f40a802e14f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1MDI5Mjk5Ng&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
-    // "https://images.unsplash.com/photo-1579600161224-cac5a2971069?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
-  ];
+  const fadeImages = [Banner1, Banner2, Banner4];
 
   return (
     <div className="hero_container">
-      <img src={Banner2} className="hero" />
+      <img src={Banner2} className="hero" alt="Banner" />
     </div>
+
+    // will be adding the sliders later
 
     // <Fade className="hero_container" {...properties}>
     //   {fadeImages.map((fadeImage, index) => (
