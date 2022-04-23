4d43514a062ec06b16117e57807796a85cb8ecd1
sadath <sadathshariff65@gmail.com>
2022-04-23 17:39:29 +0530

    Added Videos Data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,457 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    kind: "youtube#searchResult",
+    etag: "HE26cr-CGm5qngyYVQRxXGSp9Qo",
+    videoId: "MLdXYEWTC1k",
+    _id: "MLdXYEWTC1k",
+    category: "Standup",
+    publishedAt: "2021-06-10T08:30:31Z",
+    channelId: "UCkS7Vxu4PjM99w0Is6idjcg",
+    title: "Bahut Pighle Hain | Zakir khan ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "In his latest standup video, Zakir Khan talks about the little things his mom does, his thoughts on the gym, and flaunts his ...",
+    channelTitle: "Zakir Khan",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/MLdXYEWTC1k/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/MLdXYEWTC1k/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/MLdXYEWTC1k/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+  },
+
+  {
+    kind: "youtube#searchResult",
+    etag: "l1iYAHsp1ij8MsUbIpJYlC8KG-M",
+
+    _id: "vLs1v0GRR18",
+    videoId: "vLs1v0GRR18",
+    category: "Sketch",
+    publishedAt: "2019-02-18T06:55:47Z",
+    channelId: "UCzNq9i-DlDDBLjPerVzJW-A",
+    title: "Comedy Sketch - Flat Lime Soda",
+    description:
+      "The first sketch from the hilarious live sketch comedy show 'Sketchy Behaviour', We join the extremely frustrating waiter Trishul as ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/vLs1v0GRR18/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/vLs1v0GRR18/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/vLs1v0GRR18/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Kenny Sebastian",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    kind: "youtube#searchResult",
+    etag: "PXC-PS1sw0_iPLiUbPch-cxLUnk",
+    videoId: "3umAFqj6EBg",
+    _id: "3umAFqj6EBg",
+    category: "Improv",
+    channelId: "UC4zWG9LccdWGUlF77LZ8toA",
+    title: "Questions Battle with Kaneez ",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Watch the amazing battle of questions played during the mentorship of Kaneez Surka in Comicstaan Season 2 Watch Now ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/3umAFqj6EBg/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/3umAFqj6EBg/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/3umAFqj6EBg/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Amazon Prime ",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "eP4ZgO21YI9aUDf5mdabwN9YhOc",
+    _id: "sIl8vsWrD8o",
+    videoId: "sIl8vsWrD8o",
+    category: "Standup",
+    channelId: "UCkS7Vxu4PjM99w0Is6idjcg",
+    title: "When I met a Delhi Girl ",
+    description:
+      "Performance at AIB Diwas! Like, Share and Subscribe! Stay Connected: Instagram: ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/sIl8vsWrD8o/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/sIl8vsWrD8o/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/sIl8vsWrD8o/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Zakir Khan",
+  },
+
+  {
+    kind: "youtube#searchResult",
+    etag: "-cUz7iZLpu1K1b5GXHlSRNWWvBw",
+
+    kind: "youtube#video",
+    videoId: "us2HxYreQy4",
+    _id: "us2HxYreQy4",
+    category: "Sketch",
+    channelId: "UCZSNzBgFub_WWil6TOTYwAg",
+    title: "Moving into a Haunted House",
+    description:
+      "What would happen if a regular family of four, really did move into a haunted house? A lot more than you'd expect. Starring Kenny ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/us2HxYreQy4/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/us2HxYreQy4/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/us2HxYreQy4/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Netflix India",
+  },
+
+  {
+    kind: "youtube#searchResult",
+    etag: "zEPRSz3u5lK0c53WDmyDLcrwWTM",
+
+    videoId: "AQJ9zmHIC3s",
+    _id: "AQJ9zmHIC3s",
+    category: "Improv",
+
+    channelId: "UCAov2BBv1ZJav0c_yHEciAw",
+    title: "ABCD GAME - Improv Show",
+    description:
+      "IMPROV COMEDY - ABCD GAME ft. @Vidit Gujrathi @ChessBase India @VAIBHAV SETHIA",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/AQJ9zmHIC3s/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/AQJ9zmHIC3s/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/AQJ9zmHIC3s/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Samay Raina",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "iCB3XOJq0gOYTRELxcEDRX4ERyI",
+    videoId: "FC1YPRr8p2M",
+    _id: "FC1YPRr8p2M",
+    category: "Sketch",
+
+    channelId: "UCAYum5hCyfkSH5T3vSD_kwQ",
+    title: "Man vs Wild ft. BEAR GUPTA ",
+    description:
+      'This is a spoof video I made of one of my favorite shows till date "Man vs Wild". Share and subscribe if you like the video :) Follow ...',
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/FC1YPRr8p2M/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/FC1YPRr8p2M/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/FC1YPRr8p2M/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Aakash Gupta",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "99BgInVpUrLvtcQsbtZ13E0YlJE",
+
+    videoId: "qkxuFKqJXWY",
+    _id: "qkxuFKqJXWY",
+    category: "Standup",
+    channelId: "UCKnZHF24IV9PT0cdSdHoWug",
+    title: "Roommate ",
+    description:
+      "MY FOURTH STAND UP VIDEO IS HERE! DO WRITE YOUR ROOMMATE EXPERIENCES IN COMMENTS AND SHARE WITH ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/qkxuFKqJXWY/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/qkxuFKqJXWY/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/qkxuFKqJXWY/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Anubhav Singh Bassi",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "hPhzMsuptld41zUOPUiVgpybsTo",
+
+    videoId: "2Oy4HpUJSgE",
+    _id: "2Oy4HpUJSgE",
+    category: "Standup",
+
+    channelId: "UC8RrHpql7M97Lqyg9J6hSww",
+    title: "CROWD WORK IN BANGALORE ",
+    description:
+      "Releasing my first (of few) crowd-work video from the show that I did in Bangalore, on 14th September 2019 at That Comedy Club.",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/2Oy4HpUJSgE/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/2Oy4HpUJSgE/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/2Oy4HpUJSgE/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Random Chikibum",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "y-ud0E00Te8ieU7Q9nsThemSX3M",
+    _id: "fk54PRXDlUA",
+    videoId: "fk54PRXDlUA",
+    category: "Improv",
+    channelId: "UC8778hjcTGUXr4TcbDUjDbw",
+    title: "Hyderabadi Improv ",
+    description:
+      "THE IMPROV Round: NEW CHOICE Description: When an Improviser says a dialogue and the mediator/host says 'NEW CHOICE' ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/fk54PRXDlUA/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/fk54PRXDlUA/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/fk54PRXDlUA/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "FIRSTACTION",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "41tJWAk91afCY7z4LF9RFs5EgD0",
+
+    videoId: "YwGeXG4dMkM",
+    _id: "YwGeXG4dMkM",
+    category: "Sketch",
+
+    channelId: "UC8RrHpql7M97Lqyg9J6hSww",
+    title: "The Weird Job Interview ",
+    description:
+      "Job Interviews can be tough and tricky but how do you deal with an interview which is purely confusing. Watch this video and find ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/YwGeXG4dMkM/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/YwGeXG4dMkM/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/YwGeXG4dMkM/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Random Chikibum",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "y33cXO_YRwTyvNr-YIqFC_SUp0o",
+    _id: "JgVtS_n9NGw",
+    videoId: "JgVtS_n9NGw",
+    category: "Improv",
+    publishedAt: "2018-09-29T11:53:26Z",
+    channelId: "UC6a7TnbzoeggcDscJBrR0kw",
+    title: "Improv All Stars Games Night",
+    description:
+      "The Improv All Stars Games Night is an improvised show where 2 teams compete in a fun, games night styled Improv battle",
+    channelTitle: "Kaneez Surka",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/JgVtS_n9NGw/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/JgVtS_n9NGw/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/JgVtS_n9NGw/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "8qTDsQvxmFVwPj4AW6eNOqfbb6M",
+    _id: "mlwF7TabGlQ",
+    videoId: "mlwF7TabGlQ",
+    category: "Standup",
+    channelId: "UCzNq9i-DlDDBLjPerVzJW-A",
+    title: "Why the Dosa is Amazing ",
+    description:
+      'Kenny Sebastian has been touring the country to sold out shows with his special "Don\'t be that guy". In his latest clip he talks ...',
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/mlwF7TabGlQ/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/mlwF7TabGlQ/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/mlwF7TabGlQ/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Kenny Sebastian",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "coZ6PsTyFKYgl7YVJwOtb8xFMCM",
+    _id: "-lY9JdsmxTM",
+    videoId: "-lY9JdsmxTM",
+    category: "Sketch",
+
+    channelId: "UC5W8kZaY_u59fwgtjnLrBXA",
+    title: "THE BREAK-UP ",
+    description:
+      "F R U S T This sketch is a part of a Sketch that is part of SKETCHY BEHAVIOUR a full live sketch comedy show! The full video and ...",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/-lY9JdsmxTM/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/-lY9JdsmxTM/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/-lY9JdsmxTM/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Kanan Gill",
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: "t8ZEP_d6B1NdsxDdZgT3FrhOTo0",
+    _id: "t_H9wNqtLW8",
+    videoId: "t_H9wNqtLW8",
+    category: "Improv",
+
+    channelId: "UCFqMbgDyfKrLBAsTPQtAyjQ",
+    title: "Cab Driver and Passenger ",
+    description:
+      "Fb/SaadKhanOnline instagram @saadkhanfilms https://www.linkedin.com/in/saad-khan-director.",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/t_H9wNqtLW8/default.jpg",
+        width: 120,
+        height: 90,
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/t_H9wNqtLW8/mqdefault.jpg",
+        width: 320,
+        height: 180,
+      },
+      high: {
+        url: "https://i.ytimg.com/vi/t_H9wNqtLW8/hqdefault.jpg",
+        width: 480,
+        height: 360,
+      },
+    },
+    channelTitle: "Saad Khan",
   },
 ];
